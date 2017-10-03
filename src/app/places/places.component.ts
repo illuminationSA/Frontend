5a31b6c546a1493e738b39a055aa53dbd6d81cd6
Felipe Moreno <anfmorenoso@unal.edu.co>
2017-10-02 23:24:29 -0500

    Deleting unused stuff
    yeah

--- src/app/places/places.component.ts
+++ src/app/places/places.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PlacesService } from '../services/places.service';
+import { Http, Request, Headers, RequestOptions, RequestMethod } from '@angular/http';
 
 @Component({
   selector: 'app-places',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlacesComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private placesService: PlacesService,
+    private http: Http
+  ) { }
 
   ngOnInit() {
+  }
+
+  sendData( user_id ){
+    this.placesService.submitData(user_id).subscribe(
+      res => { console.log( res ) })
   }
 
 }
--- src/app/services/places.service.ts
+++ src/app/services/places.service.ts
@@ -1,8 +1,31 @@
+
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map'
 
 @Injectable()
 export class PlacesService {
 
-  constructor() { }
+  constructor( private http: Http ) { }
+
+  submitData(user_id){
+    let body = { user: user_id }
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.post('http://localhost:3000/places', body, options)
+      .map((response:Response) => {
+        console.log(response.json());
+        response.json();
+      })
+  }
+
+  // getPlaces(user_id){
+  //   let body = { user: user_id }
+  //   let headers = new Headers({ 'Content-Type': 'application/json' });
+  //   let options = new RequestOptions({ headers: headers });
+  //
+  // }
 
 }
