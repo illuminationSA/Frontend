c0a54a3d3a8ad38c73aa9823294d2b36bd21f8b1
Felipe Moreno <anfmorenoso@unal.edu.co>
2017-10-05 05:31:04 -0500

    Fin funcionan lights
    Add, edit y borrar lights

--- src/app/services/places.service.ts
+++ src/app/services/places.service.ts
@@ -18,6 +18,16 @@ export class PlacesService {
     })
   }
 
+  updateLights( light_name, light_id ){
+    let body = { name: light_name };
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.put('http://localhost:3000/lights/'+light_id, body, options)
+      .map((response: Response) => { console.log(response.json());
+    })
+  }
+
   newPlace( place_name, u_id ){
     let body = { name: place_name, user_id: u_id };
     let headers = new Headers({ 'Content-Type': 'application/json' });
@@ -27,11 +37,28 @@ export class PlacesService {
     .map((response: Response) => {console.log(response.json());})
   }
 
+  newLight( light_name, p_id ){
+    let body = { name: light_name, place_id: p_id };
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.post('http://localhost:3000/lights/', body, options)
+    .map((response: Response) => {console.log(response.json());})
+  }
+
   deletePlace( place_id ){
     let headers = new Headers({ 'Content-Type': 'application/json' });
     let options = new RequestOptions({ headers: headers });
 
     return this.http.delete('http://localhost:3000/places/'+place_id, options)
+    .map((response: Response) => {console.log(response.json());})
+  }
+
+  deleteLight( light_id ){
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.delete('http://localhost:3000/lights/'+light_id, options)
     .map((response: Response) => {console.log(response.json());})
   }
 
